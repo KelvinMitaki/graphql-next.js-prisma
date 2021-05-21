9be4da972600ec8347252fa4f9f7a64760cbece3
KelvinMitaki <mitakikelvin1@gmail.com>
2021-05-21 14:06:47 +0300

    wired up createUser mutation

--- pages/api/graphql/resolvers/user/createUser.ts
+++ pages/api/graphql/resolvers/user/createUser.ts
@@ -1,16 +1,17 @@
 import { ContextType } from "../../../../../src/types/general";
 import { MutationCreateUserArgs } from "../../generated/resolver-types";
+import { generatePasswordAsync } from "../../utils/password";
 export const createUser = async (
   _,
   { values }: MutationCreateUserArgs,
   { prisma }: ContextType
 ) => {
-  const { email, fullName, password } = values;
+  const { email, password } = values;
   const emailExists = await prisma.user.findUnique({ where: { email: email.toLowerCase() } });
   if (emailExists) {
     throw new Error("User with that email already exists");
   }
   return prisma.user.create({
-    data: values
+    data: { ...values, password: await generatePasswordAsync(password) }
   });
 };
