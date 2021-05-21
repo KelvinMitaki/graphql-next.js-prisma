5a0d02f41ce07f3731050253ebf192752bce7b04
KelvinMitaki <mitakikelvin1@gmail.com>
2021-05-21 17:46:49 +0300

    added styled components

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = () => {
-  return <div></div>;
+const Container = styled.div`
+  text-align: center;
+`;
+const Button = styled.button``;
+const ButtonComponent = () => {
+  return (
+    <Container>
+      <Button>Button</Button>
+    </Container>
+  );
 };
 
-export default Button;
+export default ButtonComponent;
